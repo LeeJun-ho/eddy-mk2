3b0caa4c25169dee4ea932223a06d13e049994af
에디[이준호] <zalban@twosun.com>
2026-04-16 17:53:40 +0900

    fix: 실행 오류 수정

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from '@mikro-orm/sqlite';
-import { Task } from './src/task/task.entity';
+import { defineConfig, ReflectMetadataProvider, SqliteDriver } from '@mikro-orm/sqlite';
+import { BaseEntityRepository } from '@libs/database/repositories/base-entity.repository';
+import { Task } from 'src/task/task.entity';
+import { TaskStepResult } from 'src/task/task-step-result.entity';
 
 export default defineConfig({
+  driver: SqliteDriver,
   dbName: './data/mk2.db',
-  entities: [Task],
-  debug: process.env.NODE_ENV !== 'production',
+  entities: [Task, TaskStepResult],
+  entitiesTs: ['./src/**/*.entity.ts'],
+  metadataProvider: ReflectMetadataProvider,
+  entityRepository: BaseEntityRepository,
+  debug: process.env.DEBUG ? process.env.DEBUG.toLowerCase() === 'true' : false,
 });
+
+
